725961aee905fc95ac606dce8829195df984d210
Ibtesamfahim07 <ibtesamfaheem7@gmail.com>
2026-02-18 15:30:47 +0500

    fix: controller import case sensitivity

--- src/routes/reminderRoutes.js
+++ src/routes/reminderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const reminderController = require('../controllers/Remindercontroller');
+const reminderController = require('../controllers/remindercontroller');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // All routes require authentication
